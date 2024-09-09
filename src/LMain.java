47c3752389c2ae184dab0972f9b7447e69a52dfd
dlqja <beomjin754@gmail.com>
2024-09-09 16:39:37 +0900

    [0909] Feat:JFrame

---
+++ src/LMain.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class LMain {
+	
+	private JFrame mainFrame;
+	
+	private void initialize() {
+		this.mainFrame = new JFrame();
+		
+	}
+	
+	public LMain() {
+		
+	}
+
+	public static void main(String[] args) {
+		LMain main = new LMain();
+		main.initialize();
+		
+	}
+
+	
+
+}
